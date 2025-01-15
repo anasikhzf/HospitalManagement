8cfde62e7bebc5420c034d274ef0edb8112e10d0
anasikhzf <anasikhzf@gmail.com>
2025-01-15 22:21:32 +0700

    anasikhzf

--- function.php
+++ function.php
@@ -4,7 +4,7 @@ session_start();
 $host       = "localhost";
 $user       = "root";
 $pass       = "";
-$db         = "hospital";
+$db         = "hospital_management";
 
 $koneksi    = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi){
